c8cfe927d05bcec81c9eab1fa75f0bd739b927c7
John D <jjd104@txstate.edu>
2019-02-24 14:20:30 -0600

    Some Entitiy Frame Work Added.

---
+++ ChickenFeed/ChickenFeed/DataContexts/ChickenDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ClassLibrary1;
+
+namespace ChickenFeed.DataContexts
+{
+    public class ChickenDb : DbContext
+    {
+        //public DbSet<Pieces>  Pieces { get; set; }
+    }
+}
---
+++ ChickenFeed/ClassLibrary1/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Book
+    {
+       public int Id { get; set; }
+
+       [Required]
+       [StringLength(255)]
+       public string Title { get; set; }
+
+       public Pieces Category { get; set; }
+
+    }
+}
