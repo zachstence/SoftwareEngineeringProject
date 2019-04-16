e109adf349ad3feaa4bbb4f55b6c27cada6d631a
John Daloni <DaloniJJD@gmail.com>
2019-04-15 19:19:45 -0500

    Added Test Files.

---
+++ Chicken.Web/Chicken.WebTests/Controllers/InventoryControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chicken.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chicken.Web.Controllers.Tests
+{
+    [TestClass()]
+    public class InventoryControllerTests
+    {
+        [TestMethod()]
+        public void AddToCartTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConfirmedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddToCartTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCartIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCartItemsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
