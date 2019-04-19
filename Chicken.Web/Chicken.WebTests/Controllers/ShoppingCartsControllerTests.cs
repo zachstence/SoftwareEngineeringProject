f46ed0b1215f0bc776d70bede24d01a5789b1089
Zach Stence <zachstence@gmail.com>
2019-04-19 16:00:59 -0500

    Setup before test method

--- Chicken.Web/Chicken.WebTests/Controllers/ShoppingCartsControllerTests.cs
+++ Chicken.Web/Chicken.WebTests/Controllers/ShoppingCartsControllerTests.cs
@@ -9,6 +9,9 @@ using System.Web.Mvc;
 using System.Web;
 using Moq;
 using Chicken.WebTests;
+using Chicken.Web.DataContexts;
+using Chicken.Web.Models;
+using Inventory;
 
 namespace Chicken.Web.Controllers.Tests
 {
@@ -17,16 +20,47 @@ namespace Chicken.Web.Controllers.Tests
     {
 
         static ShoppingCartsController controller;
+        static InventoryDb db;
+        static CartItem item;
+        static Inventory.Entities.Inventory invItem;
 
         [ClassInitialize()]
         public static void MyClassInitialize(TestContext testContext) {
             controller = new ShoppingCartsController();
             TestUtil.SetFakeControllerContext(controller);
+
+            db = new InventoryDb();
+
+            // Create test ivnentory and cart items
+            invItem = new Inventory.Entities.Inventory
+            {
+                Id = 1,
+                Name = "Test Item",
+                Cost = 1,
+                Quantity = 1
+            };
+
+            item = new CartItem
+            {
+                ItemId = Guid.NewGuid().ToString(),
+                ProductId = 1,
+                CartId = "UnitTest",
+                Product = invItem,
+                Quantity = 1,
+                DateCreated = DateTime.Now
+            };
+
+            
+
         }
 
         [TestMethod()]
         public void ReduceQuantityTest()
         {
+            var cartId = item.CartId;
+            var productId = item.ProductId;
+
+            controller.ReduceQuantity(cartId, productId);
 
         }
 
